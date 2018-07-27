12f5a0ed8034bca213aa35a9ec0831d734bbc52f
destefano1986 <destefano@sina.cn>
2018-07-27 11:58:38 +0800

    Modify MultiProcessing

--- WeiboRelation.py
+++ WeiboRelation.py
@@ -6,7 +6,8 @@ import json
 import pandas as pd
 import numpy as np
 import time
-
+import multiprocessing
+import time
 
 # 遍历指定目录，显示目录下的所有文件名
 def eachFile(filepath):
@@ -50,31 +51,40 @@ def weibo_lst_to_df(lst):
     df = pd.DataFrame(dic)
     return df
 
+def _sentiment_dealing(args):
+    df, i = args
+    sentiment_lst = []
+    print ('dealing: ' + str(i))
+    try:
+        df.loc[i,'score'] = (SnowNLP(df.loc[i, 'content'])).sentiments
+    except:
+        pass
+    sen_score = df.loc[i, 'score']
+    return sen_score
+
 def sentiment_dealing(df):
-    for i in range(0, len(df)):
-        print ('dealing: ' + str(i))
-        try:
-            df.loc[i,'score'] = (SnowNLP(df.loc[i, 'content'])).sentiments
-            if df.loc[i,'score'] > 0.66:
-                df.loc[i, 'tag'] = '积极'
-            elif df.loc[i, 'score'] < 0.33:
-                df.loc[i, 'tag'] = '消极'
-            else:
-                df.loc[i, 'tag'] = '中性'
-        except:
-            pass
-    df['tag'] = df['tag'].replace(np.nan, '中性')
+    aList = [(df, i) for i in range(len(df))]
+    p = multiprocessing.Pool(PROC_NUM)
+    bList = p.map(_sentiment_dealing, aList)
+    df['score'] = bList
     return df
 
 if __name__ == '__main__':
-    filepathC = r"C:\Users\destefano\PycharmProjects\WeiboRelationship\weibo2"
+    time_start = time.time()
+    filepathC = r"C:\Users\destefano\PycharmProjects\WeiboRelationship\weibo"
     filenames = eachFile(filepathC)
     weibo_list = readFile(filenames)
     df = weibo_lst_to_df(weibo_list)
     print ('dealing line numbers: '+str(len(df)))
-    time.sleep(5)
+    time.sleep(3)
+    PROC_NUM = 4
     df = sentiment_dealing(df)
+    df['tag'] = ['中性'] * len(df)
+    df.loc[(df['score'] > 0.66), ['tag']] = ['积极'] * len(df[(df['score'] > 0.66)])
+    df.loc[(df['score'] < 0.33), ['tag']] = ['消极'] * len(df[(df['score'] < 0.33)])
+    time_end = time.time()
     df.to_csv('WeiboEmotion.csv', encoding='utf-8')
+    print('totally cost', time_end - time_start)
 
 
 
